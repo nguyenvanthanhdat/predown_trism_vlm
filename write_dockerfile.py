712084e8897408ce5adafc01576a30aec0e85db0
datnvt <nguyenvanthanhdat1810@gmail.com>
2025-04-05 13:39:09 +0700

    Fix: limit disk sapce

--- write_dockerfile.py
+++ write_dockerfile.py
@@ -5,11 +5,20 @@ docker_image = os.getenv(
     "nvcr.io/nvidia/tritonserver:24.12-vllm-python-py3"
 )
 hf_token   = os.getenv("HF_TOKEN", "")
+repo_name = os.getenv("REPO_NAME", "")
 model_name = os.getenv("MODEL_NAME", "")
+
+# file_info = f"""FROM {docker_image}
+
+# COPY {model_name} /weights/{model_name}"""
 
 file_info = f"""FROM {docker_image}
 
-COPY {model_name} /weights/{model_name}"""
+COPY download.py /workspace/download.py
+RUN pip install huggingface-hub
+RUN python3 /workspace/download.py"""
+# Write the Dockerfile
+
 
 with open("Dockerfile", "w") as f:
     f.write(file_info)
